8ea4627f10574edb3986c9e926f82dbff91aeac9
João Antunes <joaoantunes76@protonmail.com>
2020-11-11 16:08:34 +0000

    Gerar CRUD e Model Ingredients pelo Gii
    Adicionado:
    Vistas
    Controller
    Models

---
+++ backend/views/ingredients/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Ingredients */
+
+$this->title = 'Update Ingredients: ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Ingredients', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->ingredientId]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="ingredients-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
