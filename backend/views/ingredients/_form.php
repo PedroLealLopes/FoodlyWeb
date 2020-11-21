019694aff684807ea372212a94fc60857d1a4c97
João Antunes <joaoantunes76@protonmail.com>
2020-11-21 17:52:50 +0000

    fix: remade models and views _form due to changes in data model

--- backend/views/ingredients/_form.php
+++ backend/views/ingredients/_form.php
@@ -12,8 +12,6 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'ingredientId')->textInput() ?>
-
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'stock')->textInput(['maxlength' => true]) ?>
