a4e0dd75aa540cc74c5c37830cf4c5e9f28b887e
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2021-01-13 21:31:34 +0000

    feat: removed unused func

--- frontend/modules/api/controllers/UsersController.php
+++ frontend/modules/api/controllers/UsersController.php
@@ -16,11 +16,4 @@ class UsersController extends ActiveController
       $recs = $userModel::find()->all();
       return ['total' => count($recs)];
    }
-
-   
-   public function actionRestaurant($id){
-      $userModel = new $this->modelClass;
-      $recs = $userModel::find()->where("id = $id")->all();
-      return['records'=> $recs];
-   }
 }
