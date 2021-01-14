c53260dbcea1535b39f3b0b0186da077aed71449
João Antunes <2190732@my.ipleiria.pt>
2021-01-14 16:28:05 +0000

    Created custom actionLogin that recieves email and password and returns the user account

--- frontend/modules/api/controllers/UsersController.php
+++ frontend/modules/api/controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace frontend\modules\api\controllers;
 
 use yii\rest\ActiveController;
+use Yii;
+use yii\web\Response;
 
 /**
  * MenusController implements the CRUD actions for Menus model.
@@ -15,5 +17,23 @@ class UsersController extends ActiveController
       $userModel = new $this->modelClass;
       $recs = $userModel::find()->all();
       return ['total' => count($recs)];
+   }
+
+   public function actionLogin(){
+      $request = Yii::$app->request;
+      $post    = $request->post();
+      if(isset($post["email"]) && isset($post["password"]))
+      {
+         $email = $post["email"];
+         $password = $post["password"];
+         $connection = Yii::$app->getDb();
+         $command = $connection->createCommand("
+            SELECT * 
+            FROM `user` 
+            WHERE email LIKE '$email' AND password_hash LIKE '$password'
+         ");
+         $recs = $command->queryAll();
+         return $recs;
+      }
    }
 }
