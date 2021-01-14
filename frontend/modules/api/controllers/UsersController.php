2a890af173310b9060a81a1e5c3f0f573714c75f
João Antunes <2190732@my.ipleiria.pt>
2021-01-14 22:21:43 +0000

    Update UsersController.php

--- frontend/modules/api/controllers/UsersController.php
+++ frontend/modules/api/controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace frontend\modules\api\controllers;
 
+use common\models\User;
 use yii\rest\ActiveController;
 use Yii;
 use yii\web\Response;
@@ -21,19 +22,23 @@ class UsersController extends ActiveController
 
    public function actionLogin(){
       $request = Yii::$app->request;
-      $post    = $request->post();
-      if(isset($post["email"]) && isset($post["password"]))
+      $post = $request->post();
+      if(isset($post["username"]) && isset($post["password"]))
       {
-         $email = $post["email"];
-         $password = $post["password"];
+         $username = $post["username"];
+         $password = base64_decode($post["password"]);      
+         $user = User::find()->where(['username' => $username])->one();
+         if($user->validatePassword($password))
          $connection = Yii::$app->getDb();
          $command = $connection->createCommand("
-            SELECT * 
-            FROM `user` 
-            WHERE email LIKE '$email' AND password_hash LIKE '$password'
+            SELECT id, username, email, fullname, age, alergias, genero, telefone, morada, image
+            FROM user INNER JOIN profiles ON user.id = profiles.userId
+            WHERE username LIKE '$username';
          ");
          $recs = $command->queryAll();
          return $recs;
       }
+      else
+         return null;
    }
 }
