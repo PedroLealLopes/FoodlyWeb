0fb4d7889d2b4ea6af5ac8c6b4f81fa60f7e3a02
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2021-01-29 18:06:04 +0000

    fix: added update.php file

---
+++ backend/views/staff/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Staff */
+
+$this->title = 'Update Staff: ' . $model->userId;
+$this->params['breadcrumbs'][] = ['label' => 'Staff', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->userId, 'url' => ['view', 'id' => $model->userId]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="staff-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
