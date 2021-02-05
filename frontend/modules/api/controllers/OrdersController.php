adb95cdc486fe13e0c5d0ea51b1f5b8f5b4bd7ec
PedroLealLopes <pedro.ml.lopes.98@gmail.com>
2021-02-05 18:26:19 +0000

    fix: added estado initiating at 0 in api otherwise the validation would not pass

--- frontend/modules/api/controllers/OrdersController.php
+++ frontend/modules/api/controllers/OrdersController.php
@@ -21,37 +21,40 @@ class OrdersController extends ActiveController
       return $actions;
    }
 
-   public function actionCreate(){
+   public function actionCreate()
+   {
       $request = Yii::$app->request;
       $post = $request->post();
-      if(isset($post["userId"])){
+      if (isset($post["userId"])) {
          $userId = $post["userId"];
-         $order = new Orders();      
+         $order = new Orders();
          $order->date = date('Y-m-d H:m');
          $order->userId = $userId;
-         if($order->validate()){
+         $order->estado = 0;
+         if ($order->validate()) {
             $order->save();
             $order->refresh();
             $orderId = $order->orderId;
             return $orderId;
-         }
-         else{
-           return -1;
+         } else {
+            return -1;
          }
       }
    }
 
    //custom action para saber o total de registos.
-   public function actionTotal(){
+   public function actionTotal()
+   {
       $ordersModel = new $this->modelClass;
       $recs = $ordersModel::find()->all();
       return ['total' => count($recs)];
    }
 
    //custom action que permite receber os pedidos pelo userId
-   public function actionClient($id){
+   public function actionClient($id)
+   {
       $ordersModel = new $this->modelClass;
       $recs = $ordersModel::find()->where("userId = $id")->all();
-      return['records'=> $recs];
+      return ['records' => $recs];
    }
 }
